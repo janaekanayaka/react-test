b483070cc99e242b7d745d7e7201af1aec4a2462
janaka@codecommit-at-789734661575 <janaka@blackswan-technologies.com>
2024-09-25 11:24:25 +0530

    fixed the bug

--- src/api/apiHandler.ts
+++ src/api/apiHandler.ts
@@ -1,27 +1,49 @@
 import axios from 'axios';
 
-export interface Post {
-    userId: number;
-    id: number;
-    title: string;
-    description: string;
-    image:any;
-    price:number;
-}
+// export interface Post {
+//     userId: number;
+//     _id: number;
+//     title: string;
+//     description: string;
+//     image:any;
+//     price:number;
+// }
 
 const API_URL = "https://fakestoreapiserver.reactbd.com/products";
 
-export const fetchPostByUser = async (userId: number): Promise<Post[]> => {
+// export const fetchPostByUser = async (): Promise<Post[]> => {
+//     try {
+//         const response = await axios.get<Post[]>(`${API_URL}`, {
+//         });
+//         return response.data;
+//     } catch (error) {
+//         if (axios.isAxiosError(error)) {
+//             throw new Error(`Fetching posts failed: ${error.message}`);
+//         } else {
+//             throw new Error('An unexpected error occurred');
+//         }
+//     }
+// }
+
+export interface Post {
+        userId: number;
+        _id: number;
+        title: string;
+        description: string;
+        image:any;
+        price:number;
+}
+
+export const fetchPostByUser = async (): Promise<Post[]> =>{
     try {
-        const response = await axios.get<Post[]>(`${API_URL}`, {
-            // params: { userId }
-        });
-        return response.data;
-    } catch (error) {
-        if (axios.isAxiosError(error)) {
-            throw new Error(`Fetching posts failed: ${error.message}`);
-        } else {
-            throw new Error('An unexpected error occurred');
-        }
+       const response = await axios.get<Post[]>(API_URL)
+       return response.data;
+       console.log(response.data)
+    } catch (error){
+       if (axios.isAxiosError(error)){
+         throw new Error(`Fetching faild: ${error.message}`);
+       } else {
+         throw new Error(`An unexpected error occurred`);
+       }
     }
 }
